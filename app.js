0b58fea6763e789117d5fb2bbc76c02be2a81a10
Jennifer Kelly <jenniferkelly@sbcglobal.net>
2022-10-29 16:50:43 -0700

    added closing html tag and functions

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 function myFunction () {
     alert("Hello & Welcome!");
     let helloPrompt = prompt("Have We Met?")
+    if (helloPrompt==="Yes"){
+        alert("Great! Hi again, in that case!")
+    }
+    else if (helloPrompt==="No"){
+        alert("That's okay! Thanks for visiting my site!")
+    }
+    else {
+        alert("Please try again")
+    }
 }
+
+
+myFunction(); 
