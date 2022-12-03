b34efc9bf75b62ea6b8b9dcc983fa3c49c6d0745
Jennifer Kelly <jenniferkelly@sbcglobal.net>
2022-12-02 20:26:36 -0800

    added questions in JS

--- README.md
+++ README.md
@@ -6,16 +6,28 @@
 
 Instructions
 
-[] Using a total of four JavaScript ‘prompt’ statements along the lines of our class demo, have a user answer four questions.
-[] The user’s response to each question (input) should be stored in a separate variable. Name your variables carefully and let to define your variables.
-[] Using these responses, return an alert to the user (output) that concatenates their response into some kind of reply like we did in class.
-    -Strive to have your alert for each question utilize the responses to all of the prior questions such that by the fourth question, your response would look something like, “Greetings, Iggy from Ipanema, I also like to eat bananas while on vacation in Paris.”
+[X] Create a “Top Ten” at the bottom of your HTML page as an ordered list in HTML.
 
-[] In addition, for each response, create a console.log() message that indicates the nature of the question and the user’s response, as we did in class.
-[X] Set up the HTML document <body> with <header>, <main>, and <footer> elements.
-[X] In the <main> of the file, place a <h1> tag with the content “Class 1 Lab”. 
-[]Style this element with text and background colors via an internal <style> element placed in the <head> of your document.
-[] In the HTML portion of the file, place the four questions within a series of <p> tags so that they are listed on the screen.
+[X] Convert your work experience and education summary into an unordered list using HTML
 
-[] Give each <p> tag a different text color and background color by using inline styling.
+[ ] Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+
+[ ] Indicate through an alert if the guess is “too high” or “too low”.
+
+[ ] Give the user exactly four opportunities to get the correct answer.
+After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+
+[ ] Make the user guess the answer to a question that could have many possibilities so that I can have fun with with a guessing game.
+[ ] Add a 7th question that has multiple possible correct answers that are stored in an array.
+[ ] Give the user 6 attempts to guess the correct answer.
+[ ] The guesses will end once the user guesses a correct answer or they run out of attempts.
+[ ] Display all the possible correct answers to the user.
+[ ] Consider using a loop of some sort for this question.
+
+
+[ ] Keep track of the total number of correct answers. At the end tell the user how many they got correct out of the 7 questions asked.
+
+[ ] Continue to use console.logs throughout the code to track the questions that are being asked to the user, and add addtional CSS to style your HTML page.
+
+
 
--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 let Username = prompt('Hi there, what is your name?');
 
+let correctGuesses = 0;
+
 
 alert(`Welcome ${Username}, lets play a guessing game!`);
 
@@ -11,44 +13,100 @@ alert(`Welcome ${Username}, lets play a guessing game!`);
 let questionOne = prompt(`${Username}, is coffee my favorite drink?`).toLowerCase();
 if (questionOne === 'yes' || questionOne === 'y') {
    alert(`That's correct, ${Username}, I love coffee!`)
+   correctGuesses += 1
 } else if (questionOne === 'no' || questionOne === 'n') {
    alert("That's not quite right, I love coffee!");
 } else {
    alert("That's not the answer I was looking for");
 }
+
 let questionTwo = prompt('What is my favorite season?').toLowerCase();
 if (questionTwo === "fall") {
    alert("That's correct, fall is my favorite season!");
+   correctGuesses += 1
 } else if (questionTwo === 'spring' || questionTwo === 'summer' || questionTwo === 'winter') {
    alert("That's not right, please try again");
 }
+
 let questionThree = prompt('What is my favorite kind of music?').toLowerCase();
 if (questionThree === "soul") {
    alert("That's right! Soul (especially from the 50s-60s) is my favorite kind of music!");
+   correctGuesses += 1
 } else if (questionThree === 'rock' || questionThree === 'rock & roll' || questionThree === 'rock and roll') {
    alert("That's not right, please try again");
 }
+
 let questionFour = prompt('Do I love the rain?').toLowerCase();
 if (questionFour === 'yes') {
    alert("That's correct! I love to drink coffee in the fall while listening to soul and enjoying the rain!");
+   correctGuesses += 1
 } else if (questionFour === 'no' || questionFour === 'n') {
    alert("That's not quite right, please try again");
 }
+
 let questionFive = prompt('Do I have a cat?').toLowerCase();
 if (questionFive === 'yes' || questionFive === 'y') {
    alert("That's right! I do have a cat, named Mister!");
+   correctGuesses += 1
 } else if (questionFive === 'no' || questionFive === 'n') {
    alert("That's not quite right, I do have a cat, named Mister");
 }
+
 let questionSix = prompt('Am I From the Midwest?').toLowerCase();
 if (questionSix === 'yes' || questionSix === 'y') {
    alert("That's right, I'm from Kansas!");
+   correctGuesses += 1
 } else if (questionSix === 'no' || questionSix === 'n') {
    alert("Nice guess, but I'm actually from Kansas");
 }
+
 let questionSeven = prompt('Do I Collect Books?').toLowerCase();
 if (questionSeven === 'no' || questionSeven === 'n') {
    alert("That's not quite right. I do collect (and read) books. I enjoy mostly fiction and literature.");
 } else if (questionSeven === 'yes' || questionSeven === 'y') {
    alert("That's right! I love to read and I collect books");
+   correctGuesses += 1
 }
+
+let questionEight = 'Guess a number between 1 and 10'
+
+let guesses = 4 
+
+let correctAnswer = 8;
+let response = null 
+
+while (response != correctAnswer) {
+   if (!guesses){
+      alert("You've run out of guesses, the correct answer is 8")
+      break;
+   }
+   response = parseInt(prompt(questionEight));
+   guesses -= 1
+   if (response === correctAnswer) {
+      alert("That's correct!");  
+      correctGuesses += 1 
+   } else if (response < correctAnswer) {
+      alert("That's too low, please try again");
+   } else (response > correctAnswer) {
+      alert("That's too high, please try again");
+   }
+ let questionNine = prompt('Which of the following countries do I want to visit? (guess one): Netherlands, France, Spain, Italy, Bolivia, New Zealand');
+
+   var favoriteCountries = "Denmark,"Netherlands", "France", "Spain", "Italy", "New Zealand"
+   if favoriteCountries.includes(answer1) 
+      alert("Correct!");
+      break;
+   else {
+      alert("Sorry, that's not right");
+   }   
+   }
+
+   for (var i = 0<correctAnswers.length); i++ {
+      if (response === correctAnswers[i])
+      result = true;
+   }
+
+var countryElement = document.getElementbyClassName('country');
+
+
+
